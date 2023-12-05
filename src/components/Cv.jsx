2746b9bbbf0be95d28c3a1aeb645922d982e01b1
bruno10-0 <brunobrodon75@gmail.com>
2023-12-05 12:39:04 -0300

    Quitando error de descarga cv, ahora se visualiza en CV.

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -1,20 +1,23 @@
-import { GoDownload } from "react-icons/go";
+import { MdOutlineRemoveRedEye } from "react-icons/md";
+import { LuArrowUpRight } from "react-icons/lu";
 
 const Cv = () => {
 
-    const handleDownload = () => {
-        const pdfPath = "../downloads/BunoCV.pdf";
-        const link = document.createElement('a');
-        link.href = pdfPath;
-        link.target = '_blank';
-        link.download = 'Bruno_cv.pdf';
-        link.click();
-    };
+    function redirigirACanva() {
+        // URL de Canva
+        const urlCanva ="https://www.canva.com/design/DAFrgkFzlkw/THSYnwh9gB0388FF4FU1rA/view?utm_content=DAFrgkFzlkw&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"
+        // Abrir la URL en una nueva pestaña
+        window.open(urlCanva, "_blank");
+    }
+
     return (
         <>
-            <button className="relative btnCv w-full h-full " onClick={handleDownload}>
+            <button className="relative btnCv w-full h-full "onClick={redirigirACanva}>
                 <div className="absolute top-0 left-0 m-4">
-                    <GoDownload className="text-white text-4xl" />
+                    <MdOutlineRemoveRedEye className="text-white text-4xl" />
+                </div>
+                <div className="absolute top-0 right-0 m-4">
+                    <LuArrowUpRight className="text-white text-4xl" />
                 </div>
                 <img
                     src="https://res.cloudinary.com/dzemdgvqo/image/upload/v1701559261/yw1swaeh0groeaizr7lk.png"
@@ -24,8 +27,8 @@ const Cv = () => {
                     style={{ fill: '#0092E4' }}
                 />
                 <div className="absolute bottom-0 left-0 m-4 flex flex-col justify-start items-start">
-                    <p className="text-white text-2xl font-bold">Descargar</p>
-                    <p className="text-white text-sm font-medium">Resumen</p>
+                    <p className="text-white text-2xl font-bold">Currículum</p>
+                    <p className="text-white text-sm font-medium">Visualizar</p>
                 </div>
             </button>
 
